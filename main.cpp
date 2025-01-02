18034f5c4c244448c3510d51f919386418c5fdd0
Joohjay <japhetjohnson377@gmail.com>
2025-01-02 23:13:41 +0300

    adding save word method  to a dictionary

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
-#include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Dictionary {
 private:
-    map<string, string> dictionary;
+    vector<string> words;
+    vector<string> definitions;
 
 public:
     void addWord(const string &word, const string &definition) {
-        dictionary[word] = definition;
+        words.push_back(word);
+        definitions.push_back(definition);
+    }
+
+    void printWords() {
+        for (size_t i = 0; i < words.size(); ++i) {
+            cout << words[i] << ": " << definitions[i] << endl;
+        }
     }
 };
 
 int main() {
+    Dictionary dict;
+    dict.addWord("example", "a representative form or pattern");
+    dict.addWord("test", "a procedure intended to establish the quality, performance, or reliability of something");
+
+    dict.printWords();
 
     return 0;
 }
