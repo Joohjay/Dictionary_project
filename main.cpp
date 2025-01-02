1c2b00f9cb7fe84786425e6690db9938db21b2a7
Joohjay <japhetjohnson377@gmail.com>
2025-01-02 23:14:39 +0300

    adding mechanism to search word from dictionary

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
@@ -6,28 +7,58 @@ using namespace std;
 
 class Dictionary {
 private:
-    vector<string> words;
-    vector<string> definitions;
+    string filepath;
 
 public:
-    void addWord(const string &word, const string &definition) {
-        words.push_back(word);
-        definitions.push_back(definition);
+    Dictionary(const string &path) : filepath(path) {}
+
+    void addWordToFile(const string &word, const string &definition) {
+        ofstream file;
+        file.open(filepath, ios::app);
+        if (file.is_open()) {
+            file << word << ":" << definition << endl;
+            file.close();
+        } else {
+            cerr << "Unable to open file for writing." << endl;
+        }
     }
 
-    void printWords() {
-        for (size_t i = 0; i < words.size(); ++i) {
-            cout << words[i] << ": " << definitions[i] << endl;
+    string searchWordInFile(const string &word) {
+        ifstream file(filepath);
+        string line, foundDefinition = "Word not found!";
+        if (file.is_open()) {
+            while (getline(file, line)) {
+                size_t pos = line.find(':');
+                if (pos != string::npos) {
+                    string fileWord = line.substr(0, pos);
+                    string fileDefinition = line.substr(pos + 1);
+                    if (fileWord == word) {
+                        foundDefinition = fileDefinition;
+                        break;
+                    }
+                }
+            }
+            file.close();
+        } else {
+            cerr << "Unable to open file for reading." << endl;
         }
+        return foundDefinition;
     }
 };
 
 int main() {
-    Dictionary dict;
-    dict.addWord("example", "a representative form or pattern");
-    dict.addWord("test", "a procedure intended to establish the quality, performance, or reliability of something");
+    Dictionary dict("vocabulary.txt");
 
-    dict.printWords();
+    // Add words to the file
+    dict.addWordToFile("example", "a representative form or pattern");
+    dict.addWordToFile("test", "a procedure intended to establish the quality, performance, or reliability of something");
+
+    // Search for words
+    string wordToSearch = "example";
+    cout << "Definition of " << wordToSearch << ": " << dict.searchWordInFile(wordToSearch) << endl;
+
+    wordToSearch = "test";
+    cout << "Definition of " << wordToSearch << ": " << dict.searchWordInFile(wordToSearch) << endl;
 
     return 0;
 }
